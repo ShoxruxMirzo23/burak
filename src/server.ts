42661b816b95f51697dad6e8a77e091bcdf7b101
ShoxruxMirzo23 <shohrukh_2019@mail.ru>
2024-02-26 16:05:05 +0900

    feat:Integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
+// npm run start:dev = terminal ishlatish uchun
 // Architectural pattern: NVC, DI, MVP
 
 // Design pattern: Midleware, Decotar
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL);
