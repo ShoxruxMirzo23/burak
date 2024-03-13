d523dd0740336e5abfab416902fcfd358c644d30
ShoxruxMirzo23 <shohrukh_2019@mail.ru>
2024-03-13 19:38:20 +0900

    feat: upload files into server via multer

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
-
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
 
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
@@ -32,6 +32,12 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(function (req, res, next) {
+  const sessionInstance = req.session as T;
+  res.locals.member = sessionInstance.member;
+  next();
+});
+
 /**  3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -39,4 +45,5 @@ app.set("view engine", "ejs");
 /**  4-ROUTERS **/
 app.use("/admin", routerAdmin); //SSR: EjS
 app.use("/", router); //SPA: REACT      //Midleware design pattern
+
 export default app;
