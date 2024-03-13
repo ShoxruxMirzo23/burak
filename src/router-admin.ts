af58f3fab69419e7beed14b98db083c02726cde3
ShoxruxMirzo23 <shohrukh_2019@mail.ru>
2024-03-13 19:49:19 +0900

    feat:upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -3,6 +3,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -16,9 +17,22 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Product */
-routerAdmin.get("/product/all", productController.getAllProducts);
-routerAdmin.post("/product/create", productController.createNewProducts);
-routerAdmin.post("/product/:id", productController.updateChosenProducts);
+routerAdmin.get(
+  "/product/all",
+  restaurantController.verifyRestaurant,
+  productController.getAllProducts
+);
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  makeUploader("products").array("productImages", 5),
+  productController.createNewProducts
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProducts
+);
 /** User */
 
 export default routerAdmin;
